0a8132ace43efc67006c76d91be675fc68c25981
anupuidev@gmail.com <anupuidev@gmail.com>
2023-07-04 15:52:19 +0530

    Cocktails project

---
+++ cocktails-project/src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Welcome to our Cocktail Explorer! Our cocktail application is a
+        comprehensive resource for cocktail enthusiasts and anyone looking to
+        explore the world of mixed drinks. Whether you're a seasoned bartender
+        or a casual drink enthusiast, our app provides a vast collection of
+        cocktails. We are here to enhance your drinking experience.
+        Cheers to exploring the world of cocktails!
+      </p>
+    </section>
+  );
+}
+
+export default About;
